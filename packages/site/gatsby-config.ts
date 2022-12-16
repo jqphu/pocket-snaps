46add58f1da9d9d4a95445fbf767cc85fe8f7d16
Justin Phu <me@justinphu.dev>
2022-12-17 07:15:42 +1100

    🐛 fix path for snap and website icon

--- packages/site/gatsby-config.ts
+++ packages/site/gatsby-config.ts
@@ -10,8 +10,8 @@ const config: GatsbyConfig = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'Template Snap',
-        icon: 'src/assets/logo.svg',
+        name: 'Pocket Universe',
+        icon: 'src/assets/logo.png',
         theme_color: '#6F4CFF',
         background_color: '#FFFFFF',
         display: 'standalone',
